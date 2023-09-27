eae1526a5857a2b6d3150beceb9accc92af04186
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-09-26 23:47:05 -0300

    create html e css

---
+++ js/script.js
@@ -0,0 +1,21 @@
+;(function () {
+    const p_resultado = document.getElementById('p_resultado')
+    const d_teclado = document.getElementById('d_teclado')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})()
