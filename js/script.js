efce3de00092115d82d6c50d173fd48d14b9cbff
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-09-27 10:30:30 -0300

    .

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
     const p_resultado = document.getElementById('p_resultado')
     const d_teclado = document.getElementById('d_teclado')
     
+    p_resultado.textContent = ""
+    p_resultado.textContent += "10/2"
 
+
+    
 
 
 
