5f30ffcb769c2310f48406f4f122590b9da7c0a3
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-09-29 17:18:28 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
+import { expression } from "mathjs";
+
+
 ;(function () {
     const p_resultado = document.getElementById('p_resultado')
     const d_teclado = document.getElementById('d_teclado')
     
     p_resultado.textContent = ""
     p_resultado.textContent += "10/2"
-
+    
+    alert(p_resultado.textContent)
 
     
-
-
+    
 
 
 
