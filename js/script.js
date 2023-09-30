7f3f929adf71d778414696da9be4e2f76ea8848e
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-09-30 09:22:08 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,46 @@
-import { expression } from "mathjs";
 
 
 ;(function () {
     const p_resultado = document.getElementById('p_resultado')
     const d_teclado = document.getElementById('d_teclado')
     
-    p_resultado.textContent = ""
-    p_resultado.textContent += "10/2"
+    // p_resultado.textContent = ""
+
+
+    function clicouTeclado(e){
+
+        // retorna a função se o click foi no elemento pai
+        if(e.target.getAttribute('id') === 'd_teclado'){
+            return
+        }
+        
+        // cria uma constante tecla para o valor do id
+       const tecla = parseFloat(e.target.getAttribute('id'))
+
+
+
+       // se for um numero adiciona ele ao visor / resultado 
+        if(e.target.getAttribute('class') === 'tecla num'){
+
+            p_resultado.textContent += tecla
+        }
+
+        if(e.target.getAttribute('id') === 'reset'){
+            
+            p_resultado.textContent = ''
+        }
+
+        if(e.target.getAttribute('id') === 'del'){
+            
+            [...p_resultado.textContent].pop()
+        }
+
+    }
+
+
+    d_teclado.addEventListener("click", clicouTeclado)
     
-    alert(p_resultado.textContent)
+ 
 
     
     
