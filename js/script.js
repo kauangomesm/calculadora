5d095cba7504a51c002297ad00ee871b534ac4f4
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-10-09 01:40:50 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,112 @@
 
-// ;(function () {
+;(function () {
     const p_resultado = document.getElementById('p_resultado')
     const d_teclado = document.getElementById('d_teclado')
+    const d_temas = document.getElementById('temas')
+
+
+
+    //Variaveis de cores CSS
+    const root = document.querySelector(':root')
+    const temas = [...d_temas.children]
+
+
+    function clicoutemas(e){
+
+        if(e.target.getAttribute('id') === 'tema_1'){   
+
+            root.style.setProperty('--cor-body', '#3B4664');
+            root.style.setProperty('--cor-resultado','#191F32');
+            root.style.setProperty('--cor-font-primaria','#FDFFFF');
+            root.style.setProperty('--cor-font-secundaria','#464E59');
+            root.style.setProperty('--cor-font-terciaria','#fff9ff');
+            root.style.setProperty('--cor-teclado','#252D44');
+            root.style.setProperty('--cor-teclas','#E9E3DC');
+            root.style.setProperty('--cor-teclas-hover','#ffffff');
+            root.style.setProperty('--cor-teclaEnter','#D13F30');
+            root.style.setProperty('--cor-teclaEnter-hover','#ec5949');
+            root.style.setProperty('--cor-teclaReset','#647298');
+            root.style.setProperty('--cor-teclaReset-hover','#8b9dce')
+            root.style.setProperty('--cor-box-shadow-teclas','#b4a398')
+            root.style.setProperty('--cor-box-shadow-reset','#424e73')
+            root.style.setProperty('--cor-box-shadow-enter','#922518')
+
+            temas.forEach(function (el){
+                if(el === e.target){
+                    el.classList.add('act')
+                }
+                else {
+                    el.classList.remove('act')
+                }
+            })
+            
+
+        }
+
+        if(e.target.getAttribute('id') === 'tema_2'){
+            
+            root.style.setProperty('--cor-body', '#e6e6e6');
+            root.style.setProperty('--cor-resultado','#eeeeee');
+            root.style.setProperty('--cor-font-primaria','#39382f');
+            root.style.setProperty('--cor-font-secundaria','#3a3a30');
+            root.style.setProperty('--cor-font-terciaria','#fff9ff');
+            root.style.setProperty('--cor-teclado','#d3cdcd');
+            root.style.setProperty('--cor-teclas','#e5e4e0');
+            root.style.setProperty('--cor-teclas-hover','#ffffff');
+            root.style.setProperty('--cor-teclaEnter','#c85401');
+            root.style.setProperty('--cor-teclaEnter-hover','#ff8b38');
+            root.style.setProperty('--cor-teclaReset','#388187');
+            root.style.setProperty('--cor-teclaReset-hover','#62b5bd')
+            root.style.setProperty('--cor-box-shadow-teclas','#b6a499')
+            root.style.setProperty('--cor-box-shadow-reset','#1c5c64')
+            root.style.setProperty('--cor-box-shadow-enter','#883900')
+
+            temas.forEach(function (el){
+                if(el === e.target){
+                    el.classList.add('act')
+                }
+                else{
+                    el.classList.remove('act')
+                }
+            })
+            
+
     
-    // p_resultado.textContent = ""
+
+        }
+        if(e.target.getAttribute('id') === 'tema_3'){
+
+            root.style.setProperty('--cor-body', '#17062a');
+            root.style.setProperty('--cor-resultado','#1e0836');
+            root.style.setProperty('--cor-font-primaria','#fbe23d');
+            root.style.setProperty('--cor-font-secundaria','#fff9ff');
+            root.style.setProperty('--cor-font-terciaria','#fff9ff');
+            root.style.setProperty('--cor-teclado','#1e0836');
+            root.style.setProperty('--cor-teclas','#331b4d');
+            root.style.setProperty('--cor-teclas-hover','#6b34ac');
+            root.style.setProperty('--cor-teclaEnter','#00decf');
+            root.style.setProperty('--cor-teclaEnter-hover','#94fff9');
+            root.style.setProperty('--cor-teclaReset','#56077c');
+            root.style.setProperty('--cor-teclaReset-hover','#8631b0')
+            root.style.setProperty('--cor-box-shadow-teclas','#871b9f')
+            root.style.setProperty('--cor-box-shadow-reset','#bf15f5')
+            root.style.setProperty('--cor-box-shadow-enter','#73f7ee')
+
+            temas.forEach(function (el){
+                if(el === e.target){
+                    el.classList.add('act')
+                }
+                else{
+                    el.classList.remove('act')
+                }
+            })
+        }
+
+       
+
+
+
+    }
 
 
     function clicouTeclado(e){
@@ -226,6 +329,8 @@
 
 
     d_teclado.addEventListener("click", clicouTeclado)
+
+    d_temas.addEventListener("click", clicoutemas)
     
  
 
@@ -244,4 +349,4 @@
 
 
     
-// })()
+})()
